9829875bbd5d4f48b36fa07081a0253dd20e1676
AdiosSyS <adiossysni@gmail.com>
2022-12-09 15:54:48 +0200

    fixe problems

---
+++ vueWork/src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref, reactive } from 'vue'
+
+// defineProps({
+//   msg: String
+// })
+
+const state = reactive({ symbol: "" })
+
+
+function addText(value) {
+  let arr = value.split('').shift().toUpperCase();
+
+  // state.symbol = value.split('')
+
+  console.log(value, arr);
+}
+
+
+
+</script>
+  
+<template>
+  <input v-model="text" placeholder="Text" @keypress.enter="addText(text)">
+  <p>{{state.symbol}}</p>
+</template>
+
+<style scoped lang="sass">
+
+
+</style>
